15d3a7a4450a107aaad967115c540de588b55956
Adebayo Sunday Samuel <89652234+Sarmuel001@users.noreply.github.com>
2025-01-02 21:12:16 +0100

    Add files via upload

---
+++ docs/vite.config.js
@@ -0,0 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base:"/E-commerce/",
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base:"",
+  plugins: [react()],
+})
+css: {
+  preprocessorOptions: {
+    
+  }
+}
